d030070392304305c544f9dba6410699dcca8707
mdbow22 <mdbow22@gmail.com>
2021-10-11 22:49:40 -0400

    added functionality to delete a note

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,7 @@
 //make this file a router
 const routes = require('express').Router();
 const fs = require('fs');
+const { parse } = require('path');
 const { v4: uuidv4 } = require('uuid');
 
 //import fs functions
@@ -33,6 +34,33 @@ routes.post('/', (req, res) => {
     } else {
         res.json('title or text were missing');
     }
+});
+
+routes.delete('/:id', (req, res) => {
+    //get id of note needing deleted
+    const noteID = req.params.id;
+    
+    //read database file
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if(err) {
+            console.error(err);
+        } else {
+            //parse JSON
+            const db = JSON.parse(data);
+            //find id in file
+            const noteIndex = db.map(e => e.id).indexOf(noteID);
+            //splice that index from array
+            if(noteIndex > -1) {
+                db.splice(noteIndex, 1);
+                //rewrite file with remaining notes
+                writeToFile('./db/db.json',db);
+                res.json('note successfully deleted');
+            } else {
+                //if ID was not in array, respond that that note doesn't exist
+                res.json('note not found');
+            }
+        }
+    });
 });
 
 module.exports = routes;
